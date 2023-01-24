63d161c089adfe72b6329b1b89b1495945d8ed5e
reciopaolo <paololuigigrecio@iskolarngbayan.pup.edu.ph>
2023-01-25 00:21:00 +0800

    Added Poppins Edited main.py

--- main.py
+++ main.py
@@ -14,13 +14,17 @@ from kivy import Config
 from kivy.core.window import Window
 from kivymd.tools.hotreload.app import MDApp
 from kivymd.uix.screenmanager import MDScreenManager
-
+from kivy.core.text import LabelBase
 import View.screens
 from Model.database import DataBase
 
 Config.set("graphics", "multisamples", 0)
 os.environ["KIVY_GL_BACKEND"] = "angle_sdl2"
 
+LabelBase.register(name='Poppins-Regular', fn_regular='assets/fonts/Poppins/Poppins-Regular.ttf')
+LabelBase.register(name='Poppins-SemiBold', fn_regular='assets/fonts/Poppins/Poppins-SemiBold.ttf')
+LabelBase.register(name='Poppins-Bold', fn_regular='assets/fonts/Poppins/Poppins-Bold.ttf')
+LabelBase.register(name='Poppins-Medium', fn_regular='assets/fonts/Poppins/Poppins-Medi.ttf')
 
 class Fitrex(MDApp):
     """_summary_
@@ -37,7 +41,13 @@ class Fitrex(MDApp):
         # In this method, you don't need to change
         # anything other than the application theme.
 
-        self.theme_cls.primary_palette = "Indigo"
+        self.theme_cls.theme_style_switch_animation = True
+        self.theme_cls.theme_style_switch_animation_duration = 0.8
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "DeepPurple"
+        self.theme_cls.primary_hue = "400"
+        self.theme_cls.accent_palette = "DeepPurple"
+        self.theme_cls.accent_hue = "800"
 
         database = DataBase()
         manager_screens = MDScreenManager()
@@ -54,6 +64,11 @@ class Fitrex(MDApp):
                 manager_screens.add_widget(view)
 
         return manager_screens
+
+    def switch_theme_style(self):
+        self.theme_cls.theme_style = (
+            "Dark" if self.theme_cls.theme_style == "Light" else "Light"
+        )
 
 
 if __name__ == "__main__":
