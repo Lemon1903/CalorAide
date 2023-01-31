42a9b5c351de28909e99939bc078fcda82e8bca1
Lemon1903 <99325731+Lemon1903@users.noreply.github.com>
2023-01-31 23:06:09 +0800

    Update database.py

--- Model/database.py
+++ Model/database.py
@@ -9,29 +9,16 @@ class DataBase:
     """Your methods for working with the database should be implemented in this class."""
     def __init__(self):
         self.firebase_ = firebase.FirebaseApplication("https://fitrex-bfc21-default-rtdb.asia-southeast1.firebasedatabase.app/")
-    
+
     #USERDATA is the Table
-    def get_data(self):
-        
+    def get_data_table(self):
         try:
             data = self.firebase_.get("USERDATA", '')
+            return data
         except requests.exceptions.ConnectionError:
             return None
-        return data 
 
-    def create_table(self):
-        self.data ={
-            "Username" : "rae",
-            "Password" : "try"
-        }
-        self.firebase_.post("https://fitrex-bfc21-default-rtdb.asia-southeast1.firebasedatabase.app/USERDATA", self.data)
-    
-    def add(self, userdata):
-        self.data = {
-            "Username": userdata[0],
-            "Password": userdata[1]
-        }
-        self.firebase_.post("https://fitrex-bfc21-default-rtdb.asia-southeast1.firebasedatabase.app/USERDATA", self.data)
+    def add_user_data(self, user_data):
+        self.data = {"Password": user_data[1]}
+        self.firebase_.put("USERDATA", user_data[0], self.data)
         print("Posted!")
-
-        return()
